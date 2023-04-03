5eaa1268e1a056550af960996fba5a3450f4ca28
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-04-03 15:33:28 -0400

    Create main.js

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function changeProblem() {
   addB = Math.floor(Math.random() * 10);
   document.getElementById("intA").innerText = addA;
   document.getElementById("intB").innerText = addB;
-  document.getElementById("check").innerText = "";
-  document.getElementById("answer").value = "Answer";
+  document.getElementById("check").innerText = "Type Your Answer";
+  document.getElementById("answer").value = "";
   document.getElementById("checkAns").style.display = "inline-block";
 }
 
