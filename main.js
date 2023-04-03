a1a3c1ffebb749f3682e4c6197d25f258bdfc09c
GC2023 <91566024+GithubCoder2021@users.noreply.github.com>
2023-04-03 12:50:20 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+let addA = Math.floor(Math.random() * 10);
+let addB = Math.floor(Math.random() * 10);
+
+function answer() {
+  var answer = document.getElementById("answer").value;
+  if ((+answer) == addA + addB) {
+    document.getElementById("check").innerText = "Correct!";
+  } else {
+    document.getElementById("check").innerText = "Wrong";
+  }
+  document.getElementById("checkAns").style.display = "none";
+  setTimeout(changeProblem, 2000);
+}
+
+function changeProblem() {
+  addA = Math.floor(Math.random() * 10);
+  addB = Math.floor(Math.random() * 10);
+  document.getElementById("intA").innerText = addA;
+  document.getElementById("intB").innerText = addB;
+  document.getElementById("check").innerText = "Type Your Answer";
+  document.getElementById("answer").value = "Answer";
+  document.getElementById("checkAns").style.display = "inline-block";
+}
+
+changeProblem();
